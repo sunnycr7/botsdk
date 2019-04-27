1e4556c2271a1ed434c88e297d07fe94b833a296
sundeepk <sundeep.kotari@oracle.com>
2019-04-27 09:34:10 -0500

    port update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 var staticPath = path.join(__dirname, '/app');
 app.use(express.static(staticPath));
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
